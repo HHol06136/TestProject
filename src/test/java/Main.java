91719f783d83a9873712957482d762f5c749df29
Student   HOLMAN Huw <06136@farnborough.ac.uk>
2020-11-06 10:04:47 +0000

    Initial commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,12 @@
+public class Main<pvsm> {
+    public static void main(String[] args) {
+
+
+        String greeting = "hello world!";
+
+        System.out.println(greeting);
+
+        System.out.println(greeting.length());
+
+    }
+}
